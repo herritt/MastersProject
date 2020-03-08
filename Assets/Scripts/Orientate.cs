0a85086a5f3b01b3d0eb960f6121c61dbda22473
Randy Herritt <randy.herritt@gmail.com>
2020-03-08 15:58:56 -0300

    Added text box above ship

---
+++ Assets/Scripts/Orientate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientate : MonoBehaviour
+{
+    public GameObject other;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Vector3 relativePos = other.transform.position - gameObject.transform.position;
+        //gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, Quaternion.LookRotation(-relativePos), Time.deltaTime);
+        transform.LookAt(other.transform);
+    }
+}
