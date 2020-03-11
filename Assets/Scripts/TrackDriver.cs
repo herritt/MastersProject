0d110c0223caf74648d2f9d1dd8568c2419eeca7
Randy Herritt <randy.herritt@gmail.com>
2020-03-10 22:09:15 -0300

    Corrected speed units and calcuations

--- Assets/Scripts/TrackDriver.cs
+++ Assets/Scripts/TrackDriver.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class TrackDriver : MonoBehaviour
 {
     private const int WAYPOINT_CHECK_DISTANCE = 10;
+    private const float KTS_TO_MPS = 1.944f;
 
     public GameObject[] waypoints;
     public Vector3 waypoint;
     int currentWaypointIndex = 0;
     public float shipSpeed = 11;
     GameObject ship;
+
 
     private void Awake()
     {
@@ -26,6 +28,10 @@ public class TrackDriver : MonoBehaviour
     void Start()
     {       
         waypoint = waypoints[0].transform.position;
+
+        // convert shipSpeed from kts to units per second
+        shipSpeed /= KTS_TO_MPS;
+
     }
 
     // Update is called once per frame
@@ -48,12 +54,22 @@ public class TrackDriver : MonoBehaviour
 
         }
 
-        var t = shipSpeed * Time.deltaTime;
+        //var t = shipSpeed * Time.deltaTime;
 
-        ship.transform.position = Vector3.MoveTowards(ship.transform.position, waypoint, t);
+        //float tidalSetBearing = 45;
+        //float tidalSetSpeed = 50000f; // .5 m/s or roughly 1 kt
+        //float distance = tidalSetSpeed * Time.deltaTime;
+
+        //Vector3 tidalPos = Quaternion.AngleAxis(tidalSetBearing, Vector3.up) * Vector3.forward * distance;
+
+        //Debug.Log(tidalPos);
+
+        ship.transform.position = Vector3.MoveTowards(ship.transform.position, waypoint + tidalPos, t);
 
         Vector3 relativePos = waypoint - ship.transform.position;
         ship.transform.rotation = Quaternion.Lerp(ship.transform.rotation, Quaternion.LookRotation(-relativePos), shipSpeed / 30 * Time.deltaTime);
+
+  
 
     }
 
