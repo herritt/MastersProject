d114c2d10a642c1c00646dbc1f0429618af1c6af
Randy Herritt <randy.herritt@gmail.com>
2020-07-03 09:27:33 -0300

    Began Desktop Version
    Created title scene

---
+++ Assets/Scripts/MenuButtonHover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuButtonHover : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+
+    public Color inActiveColour;
+    public Color activeColour;
+
+    public void Start()
+    {
+        text = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnMouseOver()
+    {
+        text.color = activeColour;
+    }
+
+    public void OnMouseExit()
+    {
+
+        text.color = inActiveColour;
+    }
+
+}
---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public GameObject panel;
+    public System.String newScene;
+    public float fadeInSpeed = 2f;
+    public float fadeOutSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.GetComponent<Image>().CrossFadeAlpha(0, fadeInSpeed, true);
+    }
+
+    public void TransitionOut()
+    {
+        StartCoroutine(FadeThenLoadScene());
+    }
+
+    IEnumerator FadeThenLoadScene()
+    {
+        // start fading
+        yield return StartCoroutine(FadeIn());
+        // code here will run once the fading coroutine has completed
+        SceneManager.LoadScene(newScene, LoadSceneMode.Single);
+    }
+
+    private IEnumerator FadeIn()
+    {
+        panel.GetComponent<Image>().CrossFadeAlpha(1, fadeOutSpeed, true);
+        yield return new WaitForSeconds(fadeOutSpeed);
+        
+ 
+    }
+
+}
