1cb5e23becd5941e3b9f221fd3c07449585506ca
Randy Herritt <randy.herritt@gmail.com>
2020-07-07 12:49:37 -0300

    Created an instruction manager
    Allows for clicking through slides of text and images and then offers a start button at end or presentation

---
+++ Assets/Scripts/InstructionManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InstructionManager : MonoBehaviour
+{
+    public List<String> descriptions;
+    public List<Image> images;
+    public Image image;
+    public TextMeshProUGUI description;
+
+    public GameObject previousButton;
+    public GameObject nextButton;
+    public GameObject startButton;
+
+    private int slideNumber = 0;
+
+    private void Start()
+    {
+        LoadSlide(slideNumber);
+    }
+
+    public void NextSlide()
+    {
+        slideNumber++;
+        LoadSlide(slideNumber);
+    }
+
+    public void PreviousSlide()
+    {
+        slideNumber--;
+        LoadSlide(slideNumber);
+    }
+
+    public void LoadSlide(int slideNumber)
+    {
+        image = images[slideNumber];
+        description.text = descriptions[slideNumber];
+
+        if (slideNumber == 0)
+        {
+            previousButton.SetActive(false);
+        }
+        else
+        {
+            previousButton.SetActive(true);
+        }
+
+        if (slideNumber == descriptions.Count - 1)
+        {
+            startButton.SetActive(true);
+        }
+        else
+        {
+            startButton.SetActive(false);
+        }
+
+    }
+
+}
