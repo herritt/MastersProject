3c6f503a3f4044ff75fd282ee72a9a323b75544d
Randy Herritt <randy.herritt@gmail.com>
2020-07-13 14:14:24 -0300

    Can now toggle ship avoidance arna

---
+++ Assets/Scripts/AvoidanceManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidanceManager : MonoBehaviour
+{
+    private GameObject ARNA;
+    private List<GameObject> objectsInSight = new List<GameObject>();
+    public GameObject ownship;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            RaycastHit[] allHit = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition));
+            objectsInSight.Clear();
+            foreach (RaycastHit hit in allHit)
+            {
+                if (hit.collider.gameObject.name == "ARNA_HighlightOnHover")
+                {
+                    GameObject obj = hit.collider.gameObject;
+
+                    GameObject toggleObject = obj.transform.Find("CanvasToggle").gameObject;
+
+                    if (toggleObject != null)
+                    {
+                        objectsInSight.Add(toggleObject);
+                    }
+                    
+                }
+            }
+
+            ProcessCanvasObjects(objectsInSight);
+ 
+        }
+    }
+
+    private void ProcessCanvasObjects(List<GameObject> avoidanceARNA)
+    {
+        // raycast doesn't guarantee order, so sort based on how far from ownship
+        SortedList sortedList = SortBasedOnDistanceFromOwnship(objectsInSight);
+
+        GameObject obj = (GameObject)sortedList.GetByIndex(0);
+
+        obj.SetActive(!obj.activeInHierarchy);
+
+    }
+
+    private SortedList SortBasedOnDistanceFromOwnship(List<GameObject> objects)
+    {
+        SortedList sortedList = new SortedList();
+
+        foreach(GameObject obj in objects)
+        {
+            sortedList.Add(Vector3.Distance(obj.transform.position, ownship.transform.position), obj);
+        }
+
+        return sortedList;
+    }
+
+    public static void PrintKeysAndValues(SortedList myList)
+    {
+        Debug.Log("\t-KEY-\t-VALUE-");
+        for (int i = 0; i < myList.Count; i++)
+        {
+            Debug.Log(("\t{0}:\t{1}", myList.GetKey(i), myList.GetByIndex(i)));
+        }
+    }
+}
+
---
+++ Assets/Scripts/DetectLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hitInfo))
+        {
+            Debug.Log(hitInfo.collider.gameObject.transform.parent.ToString());
+        }
+    }
+}
